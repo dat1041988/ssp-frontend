2731bc4ece31845a9a8d93d4ef5c58b1f0c114de
u220374 <reto.lehmann@sbb.ch>
2018-01-30 09:04:54 +0100

    Fixed wrong SED.

--- docker/start.sh
+++ docker/start.sh
@@ -2,7 +2,7 @@
 
 # replace static values with environment-variables
 if [ -n "$SSP_BACKEND_URL" ]; then
-    sed -i "s#http://localhost:8000/#$SSP_BACKEND_URL#g" /usr/share/nginx/html/dist/build.js
+    sed -i "s#http://localhost:8000#$SSP_BACKEND_URL#g" /usr/share/nginx/html/dist/build.js
 fi
 
 # start webserver
