114453b985b003eeb41cab96454ed3837c7f4cd5
u220374 <reto.lehmann@sbb.ch>
2018-05-18 14:17:29 +0200

    Startup with WZU env variable

--- docker/start.sh
+++ docker/start.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 echo 'starting nginx...'
+
 # replace static values with environment-variables
 if [ -n "$SSP_BACKEND_URL" ]; then
     sed -i "s#http://localhost:8000#$SSP_BACKEND_URL#g" /usr/share/nginx/html/main.*.js
+fi
+if [ -n "$WZU_BACKEND_URL" ]; then
+    sed -i "s#http://localhost:8001#$WZU_BACKEND_URL#g" /usr/share/nginx/html/main.*.js
 fi
 
 # start webserver
